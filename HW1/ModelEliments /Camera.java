4547d2bdcd4db1e0792a642aa8334bcc0c25bd99
design-kos <109647043+design-kos@users.noreply.github.com>
2023-10-10 10:30:48 +0500

    Create Camera.java

---
+++ HW1/ModelEliments /Camera.java
@@ -0,0 +1,17 @@
+package HW1.ModelEliments;
+
+import HW1.Stuff.Angle3D;
+import HW1.Stuff.Point3D;
+
+public class Camera {
+    public Point3D Location;
+    public Angle3D Angle;
+  
+    public void Rotate(Angle3D angleAction) {
+
+    }
+
+    public void Move(Point3D pointAction){
+
+    }
+}
