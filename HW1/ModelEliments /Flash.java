da59677f1d43c585edd1e709895f9e242ab632a9
design-kos <109647043+design-kos@users.noreply.github.com>
2023-10-10 10:31:13 +0500

    Create Flash.java

---
+++ HW1/ModelEliments /Flash.java
@@ -0,0 +1,20 @@
+package HW1.ModelEliments;
+
+import HW1.Stuff.Angle3D;
+import java.awt.*;
+import HW1.Stuff.Point3D;
+
+public class Flash {
+    public Point3D Location;
+    public Angle3D Angle;
+    public java.awt.Color color;
+    public float Power;
+
+    public void Rotate(Angle3D angleAction) {
+
+    }
+
+    public void Move(Point3D pointAction){
+
+    }
+}
